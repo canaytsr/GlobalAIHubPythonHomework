9d502c58a90e3d03e6e48f8eacd11922f3c7e2b8
canaytsr <76469149+canaytsr@users.noreply.github.com>
2020-12-25 19:00:55 +0300

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,36 @@
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number")
+c = input("Enter 3rd number: ")
+d = input("Enter 4st number: ")
+e = input("Enter 5st number: ")
+
+mylist=[a,b,c,d,e]
+
+for i in range(5):
+    if mylist[i].isdigit():#integer
+        mylist[i]=int(mylist[i])
+        
+    elif  mylist[i].replace(".","", 1).isdigit():#float
+        mylist[i]=float(mylist[i])
+            
+    elif  mylist[i].count("-"):
+        mylist[i]=mylist[i].replace("-","", 1)
+       
+        if mylist[i].isdigit():
+            mylist[i]=int(mylist[i])
+            mylist[i]=mylist[i]*-1
+            
+        else:
+            mylist[i]=float(mylist[i])
+            mylist[i]=mylist[i]*-1
+            
+    else:
+        mylist[i]=mylist[i]*(-1)
+        
+print(mylist)
+print('\nFirst value:',mylist[0], type(mylist[0]))
+print('Second value:{}  '.format(mylist[1]),type(mylist[1]))
+print('Third value:{}  '.format(mylist[2]),type(mylist[2]))
+print(f'Fourth value:{mylist[3]} ',type(mylist[3]))
+print(f'Fifth value:{mylist[4]}  ',type(mylist[4]))
+
